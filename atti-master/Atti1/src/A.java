791ae39dcfe5d4debec01981bec4b8646d1d5e5e
student <student@DESKTOP-KNBBNT5>
2016-11-14 14:11:12 +0900

    수정완료

--- atti-master/Atti1/src/A.java
+++ atti-master/Atti1/src/A.java
@@ -3,7 +3,7 @@ public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-            System.out.println("¿ì¾î");
+            System.out.println("ìˆ˜ì •ã…‹");
 	}
 
 }
