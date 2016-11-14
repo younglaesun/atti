67809f5003f943edc51c54e82f4d2c0504096bfc
student <student@DESKTOP-B4021AT>
2016-11-14 14:19:08 +0900

    에라이

--- atti_1/src/atti_1/A.java
+++ atti_1/src/atti_1/A.java
@@ -6,6 +6,7 @@ public class A {
 		// TODO Auto-generated method stub
           System.out.println("우아");
           System.out.println("zzzzz");
+          System.out.println("슈발 이거 모야");
 	}
 
 }
