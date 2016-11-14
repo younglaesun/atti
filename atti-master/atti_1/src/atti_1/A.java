f4055c5a5b6ff566411c8a00514e8f4ee5fb2da1
student <student@DESKTOP-VVSD8RV>
2016-11-14 14:16:19 +0900

    안녕

--- atti-master/atti_1/src/atti_1/A.java
+++ atti-master/atti_1/src/atti_1/A.java
@@ -4,7 +4,7 @@ public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          System.out.println("수정ㅋ");
+          System.out.println("aa");
 	}
 
 }
